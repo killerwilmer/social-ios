b385559503e5df96024090829bd1b8697660d007
Wilmer <killerwilmer@gmail.com>
2017-04-25 00:04:03 -0500

    facebook auth ok

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,32 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Killer: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("Killer: User cancelled Facebook authentication")
+            } else {
+                print("Killer: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Killer: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("Killer: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 

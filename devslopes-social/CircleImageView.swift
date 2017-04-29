1504ad7db2142f4a235bd1d779a65964eecbba6a
Wilmer <killerwilmer@gmail.com>
2017-04-29 13:10:39 -0500

    feed ui complete

--- devslopes-social/CircleImageView.swift
+++ devslopes-social/CircleImageView.swift
@@ -1,23 +1,27 @@
 //
-//  FancyView.swift
+//  CircleImageView.swift
 //  devslopes-social
 //
-//  Created by Wilmer Arteaga on 22/04/17.
+//  Created by Wilmer Arteaga on 28/04/17.
 //  Copyright © 2017 tinpu. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleImageView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+    }
+    
 }

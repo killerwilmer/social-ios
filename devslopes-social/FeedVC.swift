4df3100daa49c377e40b5bf0b23b06521f104bab
Wilmer <killerwilmer@gmail.com>
2017-05-02 20:13:50 -0500

    completed upload image to firebase

--- devslopes-social/FeedVC.swift
+++ devslopes-social/FeedVC.swift
@@ -14,10 +14,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var imageAdd: CircleImageView!
+    @IBOutlet weak var captionField: FancyField!
     
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
+    var imageSelected = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,6 +76,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image  = info[UIImagePickerControllerEditedImage] as? UIImage {
             imageAdd.image = image
+            imageSelected = true
         } else {
             print("KILLER: A valid image wasn't seleted")
         }
@@ -85,6 +88,31 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func postBtnTapped(_ sender: Any) {
+        guard let caption = captionField.text, caption != "" else {
+            print("KILLER: Caption must be entered")
+            return
+        }
+        guard let img = imageAdd.image, imageSelected == true else {
+            print("KILLER: An image must be selected")
+            return
+        }
+        
+        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+            let imgUid = NSUUID().uuidString
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            DataService.ds.REF_POST_IMAGES.child(imgUid).put(imgData, metadata: metadata) {(metadata, error) in
+                if error != nil {
+                    print("KILLER: Unable to upload image to firebase storage")
+                } else {
+                    print("KILLER: Successfully uploaded image to firebase storage")
+                    let downloadUrl = metadata?.downloadURL()?.absoluteURL
+                }
+            }
+        }
+    }
     
     @IBAction func signOutTapped(_ sender: Any) {
         let removeSuccessful: Bool = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
